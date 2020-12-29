37d8e68bc9f8a0a53631ca6c3493518937358011
Daniel Fernandez cacho <dani.fernandezcacho@outlook.es>
2020-12-29 10:17:40 +0100

    solucion

--- UD5/e10.cs
+++ UD5/e10.cs
@@ -6,5 +6,24 @@ namespace UD5
 {
     class e10
     {
+        public static void Taulademultiplicar()
+        {
+            int num, i;
+            int[] tabla = new int[11];
+
+            Console.WriteLine("Quin numero vols multiplicar");
+            num = Int32.Parse(Console.ReadLine());
+            for (i = 0; i < tabla.Length; i++)
+            {
+                tabla[i] = num * i;
+            }
+
+            Console.WriteLine("");
+
+            for (i = 0; i < tabla.Length; i++)
+            {
+                Console.WriteLine(tabla[i]);
+            }
+        }
     }
 }
